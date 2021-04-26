e95c5a42ef507a828d4508ee50300d086cd167d8
Aktan-web <idinex@gk.ru>
2021-04-26 18:56:11 +0600

    redesign navbar

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
 ]
 # static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
